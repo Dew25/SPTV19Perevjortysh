42bdf34a4c7c632fab03bb89d04a0993ec44fedf
none <none@ee.ee>
2020-09-23 10:11:18 +0300

    Задача решена

--- src/sptvr19perevjortysh/App.java
+++ src/sptvr19perevjortysh/App.java
@@ -5,6 +5,8 @@
  */
 package sptvr19perevjortysh;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -12,6 +14,21 @@ package sptvr19perevjortysh;
 class App {
     public void run(){
         System.out.println("=== Перевертыш ===");
-        
+        //просим пользователя ввести строку
+        System.out.print("Введите строку: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        //полученную строку преобразуем в массив char[]
+        char[] chStr = str.toCharArray();
+        //создадим новый массив для обратной строки
+        char[] rChStr = new char[str.length()];
+        //в цикле пройдем по rChStr и поместим 
+        //из chStr символы в обратном порядке
+        for (int i = 0; i < rChStr.length; i++) {
+            rChStr[i] = chStr[(chStr.length - 1) - i];
+        }
+        //преобразуем массив символов rChStr в строку
+        String rStr = new String(rChStr);
+        System.out.println("Перевертыш: "+rStr);
     }
 }
